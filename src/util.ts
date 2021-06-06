45f8e444bbdb40274ae22b7fd7c5b9de813fee10
Ringo Hoffmann <riho@live.de>
2021-06-06 11:29:31 +0200

    snackbar info on copy2clipboard

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import { showSnackbar } from './snackbarService';
+
 export function copyToClipboard(v: string): boolean {
   const inpt = document.createElement('input');
   inpt.style.position = 'absolute';
@@ -9,5 +11,9 @@ export function copyToClipboard(v: string): boolean {
   inpt.setSelectionRange(0, v.length);
   const success = document.execCommand('copy');
   document.body.removeChild(inpt);
+
+  if (success) showSnackbar('Copied to clipboard.', 'success');
+  else showSnackbar('Copying to clipboard failed. :(', 'error');
+
   return success;
 }
